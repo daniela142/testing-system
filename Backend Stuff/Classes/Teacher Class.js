3c4517adb8af29a983c128e4ffd1f7e269876936
benjaminbagala <benjaminbagala117@gmail.com>
2023-09-12 03:02:26 +1000

    added clasroom class

--- Backend Stuff/Classes/Teacher Class.js
+++ Backend Stuff/Classes/Teacher Class.js
@@ -7,6 +7,18 @@ class Teacher {
         this.lastName = lastName;
     }
 
+    createStudentObject() {
+        //Prompt student email input and store in variable studentEmail
+        //Prompt student password input and store in variable studentPassword
+        //Prompt student first name input and store in variable studentFirstName
+        //Prompt student last name input and store in variable studentLastName
+        //Prompt student starting elo and store in variable studentStartingElo
+        //Create student object (new Student(studentEmail, studentPassword, studentFirstName, studentLastName, studentStartingElo))
+        //Store object in external student array
+    }
+
+    //Method which retrieves student array
+
     getTeacherEmail() {
         return this.email;
     }
