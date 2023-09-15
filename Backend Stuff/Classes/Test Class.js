0e874f9532cae06ca2ae2e0e1832dc00d9e025f9
Victor Stamoulos <Victor.Stamoulos@gmail.com>
2023-09-15 10:54:59 +1000

    added comments

--- Backend Stuff/Classes/Test Class.js
+++ Backend Stuff/Classes/Test Class.js
@@ -1,10 +1,13 @@
 class Test{
     constructor(testName, numQuestions, description, startDate ,dueDate){
+        //the name of the created test
         this.testName = testName;
+        //the number of questions the student must answer in this test
         this.numQuestions = numQuestions;
         this.description = description;
         this.startDate = startDate;
         this.dueDate = dueDate;
+        //the array of questions the test can pull from (can be larger than numQuestions)
         const questions = [];
         questions[0] = new Question("test question", "Multiple Choice", ["A: first answer", "B: second answer"], "A", 500);
     }
