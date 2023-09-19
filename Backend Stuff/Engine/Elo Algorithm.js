c6e61403e99858f772fc973bb403da24be9335e2
SLIVAL <samuel.coa48@gmail.com>
2023-09-19 18:00:31 +1000

    Elo Algorithm and QSelect Algorithm
    Improved elo algorithm and added a question selection algorithm.

--- Backend Stuff/Engine/Elo Algorithm.js
+++ Backend Stuff/Engine/Elo Algorithm.js
@@ -3,11 +3,29 @@
 // Used for each individual question within a test
 // The userRating variable is temporary and only lasts for the duration of the test
 
-// Variables
-var userRating = 350; // Integer
+// Skill Ratings
+// 0 - 99      Bronze 1
+// 100 - 199   Bronze 2
+// 200 - 299   Silver 1
+// 300 - 399   Silver 2
+// 400 - 499   Gold 1
+// 500 - 599   Gold 2
+// 600 - 699   Platinum 1
+// 700 - 799   Platinum 2
+// 800 - 899   Diamond 1
+// 900 - 999   Diamond 2
+
+// userRating must be defined as an integer between 0 inclusive to 999 inclusive
+// questionRating must be an integer multiple of 100 between 0 to 900
+
+// EXAMPLE
+// Questions from the Bronze 1 question pool will always have a rating of 0.
+// Questions from the Bronze 2 question pool will always have a rating of 100.
+// Questions from the Gold 1 question pool will always have a rating of 400.
+// Questions from the Diamond 2 question pool will always have a rating of 900.
 
 // Constants
-const kFactor = 32; // Integer
+const kFactor = 32; // Integer influencing rating gained or lost per question answered
 
 // Chance for the user to answer the question correctly
 // Returns a number between 0 exclusive to 1 exclusive
@@ -23,22 +41,108 @@ function updateElo(questionRating, answeredCorrect)
 {
     var expectedOutcome = calculateExpected(userRating, questionRating);
     userRating = Math.round(userRating + kFactor * (answeredCorrect - expectedOutcome));
+    updateRank(userRating);
+}
+
+// Updates the user's displayed rank
+function updateRank(userRating)
+{
+    switch(true)
+    {
+        case userRating >= 0 && userRating <= 99:
+        {
+            userRank = "Bronze 1";
+            break;
+        }
+        case userRating >= 100 && userRating <= 199:
+        {
+            userRank = "Bronze 2";
+            break;
+        }
+        case userRating >= 200 && userRating <= 299:
+        {
+            userRank = "Silver 1";
+            break;
+        }
+        case userRating >= 300 && userRating <= 399:
+        {
+            userRank = "Silver 2";
+            break;
+        }
+        case userRating >= 400 && userRating <= 499:
+        {
+            userRank = "Gold 1";
+            break;
+        }
+        case userRating >= 500 && userRating <= 599:
+        {
+            userRank = "Gold 2";
+            break;
+        }
+        case userRating >= 600 && userRating <= 699:
+        {
+            userRank = "Platinum 1";
+            break;
+        }
+        case userRating >= 700 && userRating <= 799:
+        {
+            userRank = "Platinum 2";
+            break;
+        }
+        case userRating >= 800 && userRating <= 899:
+        {
+            userRank = "Diamond 1";
+            break;
+        }
+        case userRating >= 900 && userRating <= 999:
+        {
+            userRank = "Diamond 2";
+            break;
+        }
+    }
 }
 
 // Outputs an example test scenario
 function exampleTest(questionRating, answeredCorrect)
 {
+    updateRank(userRating);
+    console.log("User rank: " + userRank);
     console.log("User rating: " + userRating);
     console.log("Question rating: " + questionRating);
     console.log("The user has a " + (calculateExpected(userRating, questionRating) * 100).toFixed(2) + "% likelihood of answering this question correctly.");
+
+    const oldUserRating = userRating;
+    const oldUserRank = userRank;
     updateElo(questionRating, answeredCorrect);
+    if(answeredCorrect === true)
+    {
+        console.log("Question answered correctly.");
+    }
+    else
+    {
+        console.log("Question answered incorrectly.");
+    }
+
+    if(userRating < oldUserRating && userRank != oldUserRank)
+    {
+        console.log("Rank decreased...");
+    }
+    else if(userRating > oldUserRating && userRank != oldUserRank)
+    {
+        console.log("Rank increased!");
+    }
+
+    console.log("New user rank: " + userRank);
     console.log("New user rating: " + userRating);
     console.log("================================");
 }
 
 // ================================
-// Example test
+// EXAMPLE TEST
 // ================================
+var userRating = 350;
+let userRank;
 exampleTest(300, true);
-exampleTest(320, true);
-exampleTest(330, false);
+exampleTest(400, true);
+exampleTest(500, true);
+exampleTest(100, false);
---
+++ Backend Stuff/Engine/Question Selection Algorithm.js
@@ -0,0 +1,106 @@
+// Question Selection Algorithm
+
+// Used alongside the Elo Algorithm to select questions based on the user's skill
+
+// Variables
+// Question pools should be replaced with a database later
+var bronzeOnePool    = ["Bronze 1 Q1", "Bronze 1 Q2", "Bronze 1 Q3", "Bronze 1 Q4", "Bronze 1 Q5", "Bronze 1 Q6"];
+var bronzeTwoPool    = ["Bronze 2 Q1", "Bronze 2 Q2", "Bronze 2 Q3", "Bronze 2 Q4", "Bronze 2 Q5"];
+var silverOnePool    = ["Silver 1 Q1", "Silver 1 Q2", "Silver 1 Q3", "Silver 1 Q4"];
+var silverTwoPool    = ["Silver 2 Q1", "Silver 2 Q2", "Silver 2 Q3", "Silver 2 Q4", "Silver 2 Q5", "Silver 2 Q6"];
+var goldOnePool      = ["Gold 1 Q1", "Gold 1 Q2", "Gold 1 Q3", "Gold 1 Q4", "Gold 1 Q5"];
+var goldTwoPool      = ["Gold 2 Q1", "Gold 2 Q2", "Gold 2 Q3", "Gold 2 Q4", "Gold 2 Q5"];
+var platinumOnePool  = ["Platinum 1 Q1", "Platinum 1 Q2", "Platinum 1 Q3", "Platinum 1 Q4", "Platinum 1 Q5"];
+var platinumTwoPool  = ["Platinum 2 Q1", "Platinum 2 Q2", "Platinum 2 Q3", "Platinum 2 Q4", "Platinum 2 Q5"];
+var diamondOnePool   = ["Diamond 1 Q1", "Diamond 1 Q2", "Diamond 1 Q3", "Diamond 1 Q4", "Diamond 1 Q5"];
+var diamondTwoPool   = ["Diamond 2 Q1", "Diamond 2 Q2", "Diamond 2 Q3", "Diamond 2 Q4", "Diamond 2 Q5"];
+
+// Constants
+const sd = 100; // Standard deviation. Should always be set to 100.
+
+// Returns a random question with a rating of multiples of 100, including 0
+// The question rating can only be within a specified range from the user rating
+function selectQuestionRating()
+{
+    const floorUserRating = Math.floor(userRating / 100) * 100;
+    const rand = Math.floor(Math.random() * 100);
+
+    // 60% chance to select a question from the user's rating pool
+    if(rand >= 0 && rand <= 59)
+    {
+        return floorUserRating;
+    }
+    // 35% chance to select a question 1 standard deviation away from the user's rating pool
+    else if(rand >= 60 && rand <= 84)
+    {
+        // 50/50 chance for this question to be 1 SD above or below
+        const rand2 = Math.floor(Math.random() * 2);
+        if(rand2 == 1)
+        {
+            return floorUserRating + sd;
+        }
+        else
+        {
+            return floorUserRating - sd;
+        }
+    }
+    // 5% chance to select a question 2 standard deviations away from the user's rating pool
+    else if(rand >= 85 && rand <= 99)
+    {
+        // 50/50 chance for this question to be 1 SD above or 
+        const rand2 = Math.floor(Math.random() * 2);
+        if(rand2 == 1)
+        {
+            return floorUserRating + (sd * 2);
+        }
+        else
+        {
+            return floorUserRating - (sd * 2);
+        }
+    }
+}
+
+function selectQuestion(questionRating)
+{
+    let newQuestion;
+    switch(questionRating)
+    {
+        case 0:
+            newQuestion = bronzeOnePool[Math.floor(Math.random() * bronzeOnePool.length)];
+            break;
+        case 100:
+            newQuestion = bronzeTwoPool[Math.floor(Math.random() * bronzeTwoPool.length)];
+            break;
+        case 200:
+            newQuestion = silverOnePool[Math.floor(Math.random() * silverOnePool.length)];
+            break;
+        case 300:
+            newQuestion = silverTwoPool[Math.floor(Math.random() * silverTwoPool.length)];
+            break;
+        case 400:
+            newQuestion = goldOnePool[Math.floor(Math.random() * goldOnePool.length)];
+            break;
+        case 500:
+            newQuestion = goldTwoPool[Math.floor(Math.random() * goldTwoPool.length)];
+            break;
+        case 600:
+            newQuestion = platinumOnePool[Math.floor(Math.random() * platinumOnePool.length)];
+            break;
+        case 700:
+            newQuestion = platinumTwoPool[Math.floor(Math.random() * platinumTwoPool.length)];
+            break;
+        case 800:
+            newQuestion = diamondOnePool[Math.floor(Math.random() * diamondOnePool.length)];
+            break;
+        case 900:
+            newQuestion = diamondTwoPool[Math.floor(Math.random() * diamondTwoPoolPool.length)];
+            break;
+    }
+    return newQuestion;
+}
+
+// ================================
+// EXAMPLE TEST
+// ================================
+var userRating = 350;
+console.log(selectQuestion(selectQuestionRating()));
